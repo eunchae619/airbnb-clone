096750b1c4f86653c3a770e74e222ecb0f656062
eunchae619 <eunchae619@naver.com>
2020-04-05 23:42:43 +0900

    -

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,75 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+
+@admin.register(models.RoomType, models.Facility, models.Amenity, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+
+    """Item Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+
+    """RoomAdmin Definition"""
+
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "bath")}),
+        (
+            "More About The Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules",),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "address",
+        "guests",
+        "beds",
+        "bedrooms",
+        "bath",
+        "check_in",
+        "check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = ("city", "=host__username")
+
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    pass
